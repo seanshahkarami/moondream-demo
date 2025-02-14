d74d4f8c33b388795912e575acd18ff8e66b371d
Sean Shahkarami <sean.shahkarami@gmail.com>
2025-02-14 10:37:45 -0600

    publishing results

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
 import moondream as md
+from waggle.plugin import Plugin
 from waggle.data.vision import Camera
 from PIL import Image
+import json
 
 models = [
     "moondream-2b-int8.mf.gz",
@@ -9,7 +11,9 @@ models = [
 ]
 
 
-def main(model, camera, query):
+def run(plugin: Plugin, model: str, camera, query: str):
+    query = query.strip()
+
     print(f"Running with model={model!r} camera={camera!r} query={query!r}", flush=True)
 
     print("Loading model...", flush=True)
@@ -19,18 +23,29 @@ def main(model, camera, query):
 
     while True:
         print("Taking snapshot from camera...", flush=True)
-        image = camera.snapshot()
-        image.save("snapshot.jpg")
-        image = Image.open("snapshot.jpg")
+        snapshot = camera.snapshot()
+        snapshot.save("snapshot.jpg")
 
         print("Encoding image...", flush=True)
+        image = Image.open("snapshot.jpg")
         encoded_image = model.encode_image(image)
 
         print("Querying model...", flush=True)
-        answer = model.query(encoded_image, query)["answer"]
+        answer = model.query(encoded_image, query)["answer"].strip()
 
         print("Query:", query, flush=True)
         print("Answer:", answer, flush=True)
+
+        output = json.dumps(
+            {
+                "query": query,
+                "answer": answer,
+            },
+            separators=(",", ":"),
+        )
+
+        plugin.upload_file("snapshot.jpg", timestamp=snapshot.timestamp)
+        plugin.publish("caption", output, timestamp=snapshot.timestamp)
 
 
 if __name__ == "__main__":
@@ -39,8 +54,10 @@ if __name__ == "__main__":
     parser.add_argument("--camera", default=0)
     parser.add_argument("query")
     args = parser.parse_args()
-    main(
-        model=args.model,
-        camera=args.camera,
-        query=args.query,
-    )
+    with Plugin() as plugin:
+        run(
+            plugin=plugin,
+            model=args.model,
+            camera=args.camera,
+            query=args.query,
+        )
